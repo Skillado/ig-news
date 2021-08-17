af4b7e44977b435c968c0c360065d0dbc3639b54
skillado <drumond.guilherme@hotmail.com>
2021-08-17 20:39:37 -0300

    create services with FaunaDB

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,26 @@
 import { FaGithub } from 'react-icons/fa'
 import styles from './styles.module.scss';
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 export function SignInButton(){
-    const isUserLoggedIn = true;
-    return isUserLoggedIn ? (
-        <button type="button" className={styles.singInButton}>
-            <FaGithub color="#eba417"/>
-            Sign in with Github
-            <FiX color="#737380" className={styles.closeIcon}/>
+    const [session] = useSession();
+    console.log(session)
+    return session ? (
+        <button 
+            type="button" 
+            className={styles.singInButton}
+            onClick={() => signOut()}
+            >
+                <FaGithub color="#eba417"/>
+                {session.user.name}
+                <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ): (
-        <button type="button" className={styles.singInButton}>
+        <button
+         type="button"
+         className={styles.singInButton}
+         onClick={() => signIn('github')}>
         <FaGithub color="#04d361"/>
         Sign in with Github
     </button>
